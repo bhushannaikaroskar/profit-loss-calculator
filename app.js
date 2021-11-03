3b4c647807aae3f4444629d632feeadc5e4a936b
bhushannaikaroskar <bhushannaikaroskar@gmail.com>
2021-11-03 07:06:59 +0530

    fix: check negative and empty input value

--- app.js
+++ app.js
@@ -13,11 +13,11 @@ function calculateProfitAndLoss(initial,quantity,current){
     if(current>initial){
         const profit = (current - initial)*quantity;
         const profitPercentage = profit/(initial*quantity) * 100;
-        showOutput(`The profit is ${profit} and the profit percent is ${profitPercentage}%`);
+        showOutput(`The profit is ${profit} and the profit percent is ${profitPercentage.toFixed(2)}%`);
     }else if(initial>current){
         const loss = (initial - current)*quantity;
         const lossPercentage = loss/(initial*quantity) * 100;
-        showOutput(`The loss is ${loss} and the profit percent is ${lossPercentage}%`);
+        showOutput(`The loss is ${loss} and the loss percent is ${lossPercentage.toFixed(2)}%`);
     }else {
         showOutput("No gain no loss")
     }
@@ -27,7 +27,14 @@ function submitHandler(){
     const initial = Number(initialPrice.value);
     const qty = Number(quantity.value);
     const current = Number(currentPrice.value);
-    calculateProfitAndLoss(initial,qty,current);
+
+    if(initialPrice.value == "" || quantity.value == "" || currentPrice.value ==""){
+        showOutput("Input fields cannot be empty enter all value")
+    }else if(initial<1 || qty<1 || current<1){
+        showOutput("Value should be greater than zero")
+    } else{
+        calculateProfitAndLoss(initial,qty,current);
+    }
 }
 
 submitButton.addEventListener("click",submitHandler);
